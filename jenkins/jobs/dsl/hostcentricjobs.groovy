2ed0cf4a9acbc21862419bde26b0bee97e15b29b
johniepaulie <miranda.johnpaul15@gmail.com>
2016-03-03 15:41:33 +0800

    Update hostcentricjobs.groovy

--- jenkins/jobs/dsl/hostcentricjobs.groovy
+++ jenkins/jobs/dsl/hostcentricjobs.groovy
@@ -6,10 +6,10 @@ def projectFolderName = "${PROJECT_NAME}"
 def hostProjectRepoUrl = "ssh://jenkins@gerrit:29418/${PROJECT_NAME}/MobileApplication"
 
 // Jobs
-def deploy = freeStyleJob(projectFolderName + "/Deployment_To_Production")
+def deploy = freeStyleJob(projectFolderName + "/Deployment_To_UAT")
 
 deploy.with{
-  description("This job Deploys from git to Production.")
+  description("This job Deploys from git to UAT.")
   environmentVariables {
       env('WORKSPACE_NAME',workspaceFolderName)
       env('PROJECT_NAME',projectFolderName)
@@ -40,11 +40,29 @@ deploy.with{
 			ftp -n $HOST <<END_SCRIPT
 			quote USER $USER
 			quote PASS $PASSWD
+            		passive
 			prompt
-			cd MY.ASM.DEVOPS
+			
+		         lcd /var/jenkins_home/jobs/Hostcentric/jobs/Mainframe/jobs/Deployment_To_Production/workspace/COPYLIB
+		         cd 'PDCLIB.UAT.BASELINE.COPYLIB'
+		         mput * 
+		            
+		         lcd /var/jenkins_home/jobs/Hostcentric/jobs/Mainframe/jobs/Deployment_To_Production/workspace/DBRMLIB
+		         cd 'PDCLIB.UAT.BASELINE.DBRMLIB'
+		         mput *
+		            
+		         lcd /var/jenkins_home/jobs/Hostcentric/jobs/Mainframe/jobs/Deployment_To_Production/workspace/DCLGEN
+		         cd 'PDCLIB.UAT.BASELINE.DCLGEN'
+		         mput *
+		            
+		         lcd /var/jenkins_home/jobs/Hostcentric/jobs/Mainframe/jobs/Deployment_To_Production/workspace/SRCELIB
+		         cd 'PDCLIB.UAT.BASELINE.SRCELIB'
+		         mput *
+		         
 			quit
 			END_SCRIPT
 			exit 0
+			
 			''')
   } 
 }
